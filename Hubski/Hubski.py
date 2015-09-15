1dc3ba89c99e3e408e850d6ccabde8def5792fd2
Veen-Desktop <asvdveen@gmail.com>
2015-09-15 21:02:31 +0200

    Started Hubski Script
    First functionality done.

---
+++ Hubski/Hubski.py
@@ -0,0 +1,12 @@
+﻿import requests
+import json
+import pprint
+
+# 1) Open url, 2) Get data into a json 3) Show the pretty version  
+http = requests.get("http://api.hubski.com/publication/266825")
+data = http.json()
+print(json.dumps(data, indent=4, sort_keys=True))
+
+#print(http.headers['content-type']) in case i get more unicode errors
+
+
